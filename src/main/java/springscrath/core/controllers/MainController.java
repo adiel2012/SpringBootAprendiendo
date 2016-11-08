e8ac5ff5b49d675caf63c4765a5fd9fbc6000d19
adiel <adiel@LAPTOP-0SKMJ8DL.attlocal.net>
2016-11-08 16:31:42 -0500

    inicio

--- src/main/java/springscrath/core/controllers/MainController.java
+++ src/main/java/springscrath/core/controllers/MainController.java
@@ -1,9 +1,14 @@
 package springscrath.core.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import springscrath.core.model.entities.User;
 
 @Controller
 @RequestMapping("/main")
@@ -19,6 +24,14 @@ public class MainController {
     @RequestMapping(value = "/sample1", method = RequestMethod.GET)
     public String sample1() {
         return "pag1";
+    }
+
+    @RequestMapping(value = "/userdetail", method = RequestMethod.GET)
+    public String sample1(@ModelAttribute("mimodelo") User user, BindingResult result, @RequestParam("nom") String nom, @RequestParam("ppr") Integer ppr, ModelMap model) {
+        model.addAttribute("ppr", ppr);
+        model.addAttribute("nom", nom);
+        model.addAttribute("model", user);
+        return "details";
     }
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/details.jsp
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Sample Application JSP Page</title>
+    </head>
+
+    <body bgcolor=white>
+
+      
+
+        <br />
+        <h1>Person details</h1>
+        
+        ppr: ${ppr}
+        
+        Name: ${model.name}
+        Name: ${user.name}
+        Name: ${mimodelo.name}
+        
+        <%= new String("Hello!") %>
+
+    </body>
+</html>
